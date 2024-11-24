99768da358feedb5ea920bbf64f24e880b566f23
ThomasxLunke <thomas-sidambarom@hotmail.com>
2024-11-24 23:01:37 +0100

    :construction: updated page title

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,6 +27,12 @@ export default function Navbar() {
       {
         pathname === "/suspense" && <h1 className='font-bold lg:inline-block'>Suspense</h1>
       }
+      {
+        pathname === "/suspense/child" && <h1 className='font-bold lg:inline-block'>Suspense first child</h1>
+      }
+      {
+        pathname === "/suspense/child/child-second" && <h1 className='font-bold lg:inline-block'>Suspense second child</h1>
+      }
 
       <div className='flex items-center gap-4'>
         <ModeToggle />
